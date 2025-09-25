bc12cf40e6157b17b2fde196240f9975a2bfbdc6
seiya <seiyareal37@gmail.com>
2025-09-25 11:59:32 +0800

    created login form

---
+++ login.js
@@ -0,0 +1,46 @@
+function signin(){
+     let email = document.getElementById("email").value;
+     let password = document.getElementById("password").value;
+
+
+     console.log(" You Entered the email : " +email );
+     console.log(" You entered the password : " + password );
+
+     if (email==="seiyareal37@gmail.com"){
+        console.log("email is correct");
+
+     }else{
+        console.assert;log(" email is inccorect.");
+        document.getElementById("error-asg").innerText = "Your email is incoorect.";
+
+
+
+
+
+
+
+
+     }
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
