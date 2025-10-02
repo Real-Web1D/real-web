14cd0eecb3c67d48d255706a531c880a5a24835b
seiya <seiyareal37@gmail.com>
2025-10-02 11:55:34 +0800

    im not done

--- login.js
+++ login.js
@@ -1,50 +1,41 @@
-function signin(){
-     let email = document.getElementById("email").value;
-     let password = document.getElementById("password").value;
+  // Real,Seiya 1 D
+ function login() {
+   let email = document.getElementById("email").value;
+   let password = document.getElementById("password").value;
+
+   console.log("You enter:" + " " + email);
+   console.log("You enter:" + " " + password);
+
+      if(email==="seiyareal37@gmail.com"){
+       console.log("Your email is correct " + email);
+      } else{
+       console.log("Your email is incorrect")
+      }
+
+      if(password==="seiya123"){
+       console.log("Your password is correct " + password);
+      } else{
+       console.log("Your pasword is incorrect");
+      } 
+
+      if(email==="seiyareal37@gmail.com" && password==="seiya123"){
+         console.log("Your email and password is correct");
+      }
+      if(email!=="seiyareal0906@gmail.com" && password!=="seiya123"){
+          console.log("Your email and password is incorrect");
+      }
+      if(email!=="seiyareal37@gmail.com" && password==="seiya123"){
+          console.log("Your email is incorrect");
+      }
+      if(email==="seiyareal37@gmail.com" && password!=="seiya123"){
+          console.log("Your password is incorrect");
+      }
+
+      
 
 
-     console.log(" You Entered the email : " +email );
-     console.log(" You entered the password : " + password );
-
-     if (email==="seiyareal37@gmail.com"){
-        console.log("email is correct");
-
-     }else{
-        console.assert;log(" email is inccorect.");
-        document.getElementById("error-asg").innerText = "Your email is incoorect.";
-
-     if (pass==="seiya_123"){
-      console.log("Your password is correct.");
-       }else{
-         console.log("Your password is incorrect.");
-         document.getElementById("error").innerHTML = "Your password is incorrect.";
-
-
-
-
-      //Email:seiyareal37@gmail.com
-      //Password:  Seiya_123
-       }
-
-
-
-
-
-
-     }
-
-
-
-
-
-
-
-
-
-
-
-
-}
+      //Email: seiyareal37@gmail.com // Error Message: your email is incorrect
+      //Password: Seiya123//Error Message: your password is incorrect
 
 
 
@@ -55,3 +46,4 @@ function signin(){
 
 
 
+   }
