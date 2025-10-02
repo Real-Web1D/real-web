33a6888df3f1609f844ce3353540bb4d4e1e8c90
seiya <seiyareal37@gmail.com>
2025-10-02 13:47:24 +0800

    im not done

--- login.js
+++ login.js
@@ -1,49 +1,41 @@
-  // Real,Seiya 1 D
- function login() {
-   let email = document.getElementById("email").value;
-   let password = document.getElementById("password").value;
+  //Real Seiya 1 D
+ function signin(){
+     let email = document.getElementById("email").value;
+     let password = document.getElementById("password").value;
 
-   console.log("You enter:" + " " + email);
-   console.log("You enter:" + " " + password);
 
-      if(email==="seiyareal37@gmail.com"){
-       console.log("Your email is correct " + email);
-      } else{
-       console.log("Your email is incorrect")
-      }
+     console.log(" You Entered the email : " +email );
+     console.log(" You entered the password : " + password );
 
-      if(password==="seiya123"){
-       console.log("Your password is correct " + password);
-      } else{
-       console.log("Your pasword is incorrect");
-      } 
+     if (email==="seiyareal37@gmail.com"){
+        console.log("email is correct");
+  
 
-      if(email==="seiyareal37@gmail.com" && password==="seiya123"){
-         console.log("Your email and password is correct");
-      }
-      if(email!=="seiyareal0906@gmail.com" && password!=="seiya123"){
-          console.log("Your email and password is incorrect");
-      }
-      if(email!=="seiyareal37@gmail.com" && password==="seiya123"){
-          console.log("Your email is incorrect");
-      }
-      if(email==="seiyareal37@gmail.com" && password!=="seiya123"){
-          console.log("Your password is incorrect");
-      }
-
+        if (pass==="Seiya_123"){
+      console.log("Your password is correct.");
       
+      }else{
+        console.assert;log(" email is inccorect.");
+        document.getElementById("error-asg").innerText = "Your email is incoorect.";
 
-
-      //Email: seiyareal37@gmail.com // Error Message: your email is incorrect
-      //Password: Seiya123//Error Message: your password is incorrect
-
-
-
-
-
-
+   
+         console.log("Your password is incorrect.");
+         document.getElementById("error").innerHTML = "Your password is incorrect.";
 
 
 
+       }
 
-   }
+
+
+     }
+
+
+
+
+     //Email:seiyareal37@gmail.com
+ //Password:  Seiya_123
+
+
+    }
+ 
